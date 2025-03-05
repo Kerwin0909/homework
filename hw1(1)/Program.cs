59d9c65c36b164e0f0478f43ec93614928cbb7d0
shekawai <shekawai@outlook.com>
2025-03-06 00:21:47 +0800

    first

---
+++ hw1(1)/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("你好！我是佘嘉煒");
+            //test
+        }
+    }
+}
---
+++ hw1(2)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int up;//上限
+            int down;//下限
+            Console.WriteLine("请输入上限");
+            up = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入下限");
+            down = int.Parse(Console.ReadLine());
+            int count = 0;//判断是否等于0个数
+            for (int i=down; i <= up; i++) {
+                int p = -1;//判断是否为素数
+                for (int j = 2; j < i; j++) {
+                    if (i % j == 0)//不是素数则跳出循环
+                    {
+                        p = 0;
+                        break;
+                    }
+                }
+                if (p != 0) {
+                    Console.Write(i+" ");
+                    count++;
+
+                }
+                if (count == 10)
+                {
+                    Console.WriteLine();
+                    count = 0;
+                }
+            }
+        }
+    }
+}
