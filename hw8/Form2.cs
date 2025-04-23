cc9d7fdfc8f27d1c8ca800e417d3baafda9737e7
Kerwin0909 <shekawai@outlook.com>
2025-04-23 23:22:06 +0800

    Add files via upload

---
+++ hw8/Form2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace hw8
+{
+    public partial class Form2: Form
+    {
+        private int i = 0;
+        private string[] data;
+        private string[] data1;
+        private int num;
+        public Form2()
+        {
+            InitializeComponent();
+            button1.Click += button1_Click;
+            connect d = new connect();
+            data = d.Data();
+            data1 = d.Data1();
+            num= d.num();
+            label1.Text = data[i];
+            label2.Text = "";
+        }
+        private async void button1_Click(object sender, EventArgs e) {
+            if (textBox1.Text == data1[i])
+            {
+                label2.Text = "Good!";
+                await Task.Delay(1000);
+                i++;
+                label1.Text = data[i];
+                textBox1.Text = "";
+                label2.Text = "";
+            }
+            else 
+            {
+                label2.Text = "The answer is "+ data1[i];
+                await Task.Delay(2500);
+                i++;
+                label1.Text = data[i];
+                textBox1.Text = "";
+                label2.Text = "";
+            }
+            if (i == num)
+            {
+                label1.Text = "";
+                label2.Text = "Congratulation all the exercises";
+                await Task.Delay(2500);
+                this.Close();
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ hw8/connect.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace hw8
+{
+    class connect
+    {
+        SqlConnection con1;
+        SqlDataReader rd;
+        public  bool TestConSQL() { 
+            string con = "Data Source=MSI;Initial Catalog=recite_word;Integrated Security=True";
+            con1 = new SqlConnection(con);
+            con1.Open();
+            if (con1.State == ConnectionState.Open)
+                return true;
+            else
+                return false;
+            }
+        int i = 0;
+        string[] result1 = new string[99];
+        public string [] Data()
+        {
+            string con = "Data Source=MSI;Initial Catalog=recite_word;Integrated Security=True";
+            string[]result = new string[99];
+            con1 = new SqlConnection(con);
+            con1.Open();
+            string sql = "SELECT Chinese,English FROM recite";
+            SqlCommand cmd = new SqlCommand(sql,con1);
+            rd= cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                result[i] = rd["Chinese"].ToString();
+                result1[i] = rd["English"].ToString();
+                i++;
+            }
+            return result;
+        }
+        public string[] Data1() {
+            return result1;
+        }
+        public int num()
+        {
+            return i;
+        }
+
+    }
+}
